add51079521978a3c0e78165313b6f7ddcd101cb
C. Titus Brown <titus@idyll.org>
2014-08-03 15:03:24 -0400

    updated to .txt

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.mathjax']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.txt'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
