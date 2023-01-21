b9d1419ffc7b383a0fdb9883c2c5bcbd09784d57
C. Titus Brown <titus@idyll.org>
2023-01-21 10:31:07 -0800

    fix markdown rendering

--- conf.py
+++ conf.py
@@ -31,7 +31,10 @@ import guzzle_sphinx_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax', 'guzzle_sphinx_theme', 'sphinxcontrib.contentui', 'sphinx_markdown_tables']
+
+extensions = ['sphinx.ext.mathjax', 'guzzle_sphinx_theme',
+              'sphinxcontrib.contentui', 'sphinx_markdown_tables',
+              'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -261,8 +264,8 @@ texinfo_documents = [
 #-----
 # Markdown support:
 
-from recommonmark.parser import CommonMarkParser
+#from recommonmark.parser import CommonMarkParser
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+#source_parsers = {
+#    '.md': CommonMarkParser,
+#}
